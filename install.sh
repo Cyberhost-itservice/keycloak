33a1396409b299aa4b4dba6b8f9d2b5e83535f5f
cyberhost <103827425+Cyberhost-itservice@users.noreply.github.com>
2022-11-20 11:13:17 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# when live system 
+#sed -i 's/main/main universe multiverse/g' /etc/apt/sources.list
+#	echo "deb http://"${LOCAL_CACHE:+${LOCAL_CACHE}/}"de.archive.ubuntu.com/ubuntu/ focal-updates main universe multiverse" >> /etc/apt/sources.list
+#	echo "deb http://"${LOCAL_CACHE:+${LOCAL_CACHE}/}"security.ubuntu.com/ubuntu/ focal-security main universe multiverse" >> /etc/apt/sources.list
+#
+apt update 
+apt install git ansible -y 
+sed -i "188s/# command_warnings = False/command_warnings = False/1" /etc/ansible/ansible.cfg 
+
+ansible-galaxy collection install community.mysql
+ansible-galaxy collection install community.general 
+ansible-galaxy collection install community.crypto
+ansible-pull -U https://code.hattec.de/git/alexander.lewrick/Ansible-Keycloak20.git
