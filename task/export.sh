01d7e58d4c0989f5062569df4e35c4f6b670866c
cyberhost <103827425+Cyberhost-itservice@users.noreply.github.com>
2023-01-31 12:52:29 +0100

    Create export.sh

---
+++ task/export.sh
@@ -0,0 +1,7 @@
+# next create user for keycloak
+cd /opt/keycloak/bin
+
+# when the export command not funct - got to the path /opt/keyclaok20.0/bin and push new
+
+export KEYCLOAK_ADMIN=keyadmin
+export KEYCLOAK_ADMIN_PASSWORD=keypass
