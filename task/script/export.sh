5d8226c9b51426ba56a6e2ccd7729176deb78ce8
cyberhost <103827425+Cyberhost-itservice@users.noreply.github.com>
2023-02-01 13:53:40 +0100

    Update export.sh

--- task/script/export.sh
+++ task/script/export.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # ip adress change in config
 
-ipadr=$(hostname -I)
+#ipadr=$(hostname -I)
 #echo $ipadr 
-sed -i '26s/.*/'ip="$ipadr"'/' /opt/keycloak-20.0.3/conf/keycloak.conf
-sed -i '41s/.*/'hostname="$ipadr"'/' /opt/keycloak-20.0.3/conf/keycloak.conf
+#sed -i '26s/.*/'ip="$ipadr"'/' /opt/keycloak-20.0.3/conf/keycloak.conf
+#sed -i '41s/.*/'hostname="$ipadr"'/' /opt/keycloak-20.0.3/conf/keycloak.conf
 
 # next create user for keycloak
 cd /opt/keycloak-20.0.3/bin
